caef2e6166d493e8acbcd4f911996416195f7150
Ju5tADeve10per <t0aruka1hat5u5ya@gmail.com>
2026-03-18 17:35:52 +0900

    feat: add code to fetch languages via URL and total code size

--- update_stats.py
+++ update_stats.py
@@ -6,16 +6,21 @@ url = f"https://api.github.com/users/{username}/repos"
 repos = requests.get(url).json()
 
 repo_count = len(repos)
-
 stars = 0
+total_bytes = 0
 languages = set()
 
 for repo in repos:
     stars += repo["stargazers_count"]
 
-    if repo["language"]:
-        languages.add(repo["language"])
+    # Retrieve lang data and num of bytes
+    langs = requests.get(repo["languages_url"]).json()
+    for lang, bytes_count in langs.items():
+        languages.add(lang)
+        total_bytes += bytes_count
+
 
 print("repos:", repo_count)
 print("stars:", stars)
-print("languages:", " ".join(list(languages)))
+print("languages:", list(languages))
+print("total bytes:", total_bytes)
